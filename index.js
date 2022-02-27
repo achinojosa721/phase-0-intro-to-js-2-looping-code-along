fe0ca984057e3ce9219b8a6edacfc873a0d41b50
Alberto Hinojosa <ahinojosa721@gmail.com>
2022-02-26 21:02:38 -0600

    commit tree f5bf433c3b8041f4d99ee7e697a83ed3dd722e04

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards (names, event) {
+    let newArray = [];
+    for (let i = 0; i < names.length; i++) {
+        let message = (`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+        newArray.push(message);
+}
+return newArray; }
+
+writeCards([ "Guadalupe", "Ollie", "Aki"], "surprise");
+
+function countDown(){
+    let number = 10; 
+    while (number >= 0){
+        console.log(number--);
+    }
+}
+countDown(10);
